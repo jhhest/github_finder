4a684fb76d5298086ecde63742e69007098b61e4
Jan van Hest <janvanhest@outlook.com>
2019-12-22 18:18:47 +0100

    Expressions & Conditionals In JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React!</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+    return (
+      <Fragment>
+        <h1>My BS App!</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello From {showName && name }</h1>}
+      </Fragment>   
+    );
+  }
 }
 
 export default App;
