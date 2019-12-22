7bb45e35e6418be4f11187cd7ab29b0d89214996
Jan van Hest <janvanhest@outlook.com>
2019-12-22 21:44:24 +0100

    10. Components, Props & PropTypes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
     return (
-      <Fragment>
-        <h1>My BS App!</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello From {showName && name }</h1>}
-      </Fragment>   
+      <nav className='navbar bg-primary'>
+        <Navbar title='Github Finder - Study Jan van Hest'/>
+      </nav>   
     );
   }
 }
