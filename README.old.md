ae049fc0321747a2fcdfd7d8abaee29c427f7cdc
Jan van Hest <janvanhest@outlook.com>
2019-12-24 01:36:22 +0100

    Getting started with component state

--- README.old.md
+++
@@ -1,5 +0,0 @@
-# github_finder
-
-Course i followed. 
-https://www.traversymedia.com
-https://www.udemy.com/course/modern-react-front-to-back/
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/user/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <nav className='navbar bg-primary'>
-        <Navbar title='Github Finder - Study Jan van Hest'/>
-      </nav>   
+      <div className="App">
+        <Navbar title='Github Finder Study Project' icon='fab fa-github' />
+        <UserItem />
+        </div>
     );
   }
 }
